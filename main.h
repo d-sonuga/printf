b1c8f0c8e7e43464068a5ae06c8168d400935aa3
Priscaezefili <priscaezefili@gmail.com>
2023-08-22 15:40:57 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,58 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/* Flags */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* Sizes */
+#define S_LONG 2
+#define S_SHORT 1
+
+/**
+ * struct fmt - Represents a format specifier.
+ * @fmt: Defines how data should be printed or formatted.
+ * @fn: The function associated with the format specifier.
+ */
+
+struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
+
+typedef struct fmt fmt_t;
+
+/* Core printf function */
+int _printf(const char *format, ...);
+
+/* Handlers for format specifiers */
+typedef int (*specifier_handler)(va_list, char[], int, int, int, int);
+
+int print_char(va_list types, char buffer[],
+			int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+			int flags, int width, int precision, int size);
+
+/* ... Other specifier handlers ...*/
+
+/* printable */
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+int is_printable(char c);
+int append_hexa_code(char c, char buffer[], int index);
+int is_digit(char c);
+
+#endif /* MAIN_H */
